94712913fde416d6ce1ca33725200792fd9fcd97
DyerMandujano <dyer0206@gmail.com>
2023-07-04 12:10:45 -0500

    Manejando errores

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs'
+import { Observable, catchError, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -15,7 +15,18 @@ export class CountriesServices {
         //*EN ESTA LINEA ESTAMOS DEFINIENDO EL RETORNO DEL METODO GET QUE SEA UN ARREGLO DE TIPO INTERFACE 'Country'
         //!NOTA: SI NO HAY NINGUN 'suscribe' ESTA PETICIÓN SOLO SE ESTÁ DEFINIENDO MÁS NO EJECUTANDOSE.
         //!ES DECIR, EN EL MOMENTO QUE NOS 'SUSCRIBRAMOS' AL 'OBSERVABLE' EN ESE INSTANTE RECIEN SE REALIZARÁ LA PETICIÓN GET HTTP.
-        return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`);
+        
+        return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`).
+        //?Los OBSERVABLES tienen un metodo llamado 'pipe', este nos sirve para especificar diferentes operadores de rxjs. Los operadores rxjs nos permiten personalizar/transformar el flujos de datos que que retorna el observable.
+        //!NOTA: SI SE UTILIZA UN PIPE, YA SEA : MAP, TAP, CATCH_ERROR, ES OBLIGATORIO QUE SE RETORNE EL MISMO TIPO DE DATO QUE EL OBSERVABLE O EN TODO CASO SE RETORNE UN OBSERBABLE VACIO YA QUE ESTOS PIPES ESTAN ENLAZADOS AL OBSERVABLE. 
+        pipe(
+            //?El metodo 'of' sirve para construir un OBSERVABLE basado en los argumentos que se le pase.
+            //ES DECIR, EN ESTA LINEA ATRAPA EL ERROR Y POR CONSOLA NOS DA TODA LA INFORMACIÓN DE ESTE Y RETORNA UN OBSERVABLE VACIO
+        catchError(error => {
+            console.log(error);
+            return of([]);
+        })
+        );
     }
     
 }
